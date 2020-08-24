972f0a8464b66a2d477d0ff596d6ec501db48150
M Haidar Hanif <git@mhaidarhanif.com>
2020-08-25 02:42:07 +0700

    Update home page content

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -6,7 +6,11 @@ const PageHome = () => {
   return (
     <Page>
       <Hero heading='Welcome to Template' />
-      <h2>Template is a starter kit project to build a complete web app.</h2>
+      <h2>
+        Template is a quick starter kit project to build a complete web app with
+        JavaScript, Node.js, React, Redux, Express, MongoDB, PostgreSQL, and
+        REST API.
+      </h2>
       <p>
         <Link to='/about'>Visit about page to learn more</Link>
       </p>
