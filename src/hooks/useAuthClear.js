a667f364d4116b4955aefbf5dbebf8d83f5675e6
M Haidar Hanif <mhaidarhanif@gmail.com>
2020-09-10 03:26:49 +0700

    Create custom hook to clear auth store

--- src/hooks/useAuthClear.js
+++ src/hooks/useAuthClear.js
@@ -2,16 +2,15 @@ import { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { clear } from '../redux/actions/auth'
 
-function useAuthClear() {
+const useAuthClear = () => {
   const error = useSelector((state) => state.auth.error)
   const dispatch = useDispatch()
-
   const [isFirstTime, setIsFirstTime] = useState(false)
 
   useEffect(() => {
     !isFirstTime && error && dispatch(clear())
     setIsFirstTime(true)
-  }, [isFirstTime, error, dispatch, setIsFirstTime])
+  }, [isFirstTime, setIsFirstTime, error, dispatch])
 }
 
 export default useAuthClear
